bd73c5ef62710b291365e94ae5ce13ad94ef10e6
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-07-26 14:56:13 -0700

    updated pseudocode

---
+++ index.js
@@ -0,0 +1,13 @@
+// import fs, inquirer and other npm packages
+
+const fs = require('fs');
+
+// ask inquirer questions using prompt (must be run BEFORE generate content and write file - CANNOT PROGRESS UNTIL COMPLETION)
+    // what text characters do you want (up to 3) (edge case for more than 3?)
+    // text color (keyword or hexidecimal) (edge case for entering non-color?)
+    // list of shapes (circle, triangle, square - multiple choice DO NOT USE LIST)
+    // shape color (keyword or hexidecimal) (edge case for entering non-color? possibly use jest?)
+
+// generate content of the svg file 
+
+// write the output file
