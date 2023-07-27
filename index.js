e6ed43cc96e892272b3332a3b0503d2dfe43d153
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-07-26 21:12:40 -0700

    created inquirer questions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// import shape classes
+const { Triangle, Circle, Square } = require('./lib/shapes');
 // import fs, inquirer and other npm packages
 const inquirer = require('inquirer');
 const fs = require('fs');
@@ -7,6 +9,40 @@ const fs = require('fs');
     // text color (keyword or hexidecimal) (edge case for entering non-color?)
     // list of shapes (circle, triangle, square - multiple choice DO NOT USE LIST)
     // shape color (keyword or hexidecimal) (edge case for entering non-color? possibly use jest?)
+
+const shapeQuestions = [
+    {
+        type: 'input',
+        message: 'What text characters do you want (up to 3)?',
+        name: 'text',
+        // create validation to check for 3 or less characters
+        validate: function (input) {
+            if (input.length > 3) {
+                return 'Please enter 3 or less characters';
+            }
+            return true;
+        }
+    },
+
+    {
+        type: 'input',
+        message: 'What color do you want the text to be?',
+        name: 'textColor',
+    },
+
+    {
+        type: 'list',
+        message: 'What shape do you want for your logo?',
+        name: 'shape',
+        choices: ['Triangle', 'Circle', 'Square']
+    },
+
+    {
+        type: 'input',
+        message: 'What color do you want the shape to be?',
+        name: 'shapeColor',
+    }
+]
 
 // generate content of the svg file
     // generate svg tag
